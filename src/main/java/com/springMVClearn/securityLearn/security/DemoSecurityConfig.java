eba978f2919ceccc86c6f44a542e4a019aeb371a
Muhammad Karimov <shermuhammad.karimov2004@mail.ru>
2024-07-09 10:41:56 +0500

    connected user db

--- src/main/java/com/springMVClearn/securityLearn/security/DemoSecurityConfig.java
+++ src/main/java/com/springMVClearn/securityLearn/security/DemoSecurityConfig.java
@@ -4,37 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test1234")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test1234")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test1234")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-        UserDetails shermukhammad = User.builder()
-                .username("shermukhammad")
-                .password("{noop}test1234")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
 
-        return new InMemoryUserDetailsManager(john, mary, susan, shermukhammad);
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -52,4 +33,30 @@ public class DemoSecurityConfig {
 
         return http.build();
     }
+
+    //    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test1234")
+//                .roles("EMPLOYEE")
+//                .build();
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test1234")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test1234")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//        UserDetails shermukhammad = User.builder()
+//                .username("shermukhammad")
+//                .password("{noop}test1234")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan, shermukhammad);
+//    }
 }
